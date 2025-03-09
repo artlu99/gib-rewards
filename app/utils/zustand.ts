99c875ab1f33ff15ecd73d955e99eef5aa76fb8b
artlu99 <alu99.sm@gmail.com>
2025-03-08 19:23:58 -0500

    persist store

--- app/utils/zustand.ts
+++ app/utils/zustand.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import type { SmoothScores } from "~/utils/smoothScores";
 import type { LeaderboardCastInfo } from "~/utils/whistles";
 
@@ -11,19 +12,24 @@ type BearStore = {
   addExcludedCast: (castHash: string) => void;
 };
 
-export const useBearStore = create<BearStore>()((set, get) => ({
-  casts: [],
-  setCasts: (casts: LeaderboardCastInfo[]) => set({ casts }),
-  smoothScores: {
-    nRaw: 0,
-    sumRaw: 0,
-    meanRaw: 0,
-    stdevRaw: 0,
-    sumSmooth: 0,
-    items: [],
-  },
-  setSmoothScores: (smoothScores: SmoothScores) => set({ smoothScores }),
-  excludedCasts: [],
-  addExcludedCast: (castHash: string) =>
-    set({ excludedCasts: [...get().excludedCasts, castHash] }),
-}));
+export const useBearStore = create<BearStore>()(
+  persist(
+    (set, get) => ({
+      casts: [],
+      setCasts: (casts: LeaderboardCastInfo[]) => set({ casts }),
+      smoothScores: {
+        nRaw: 0,
+        sumRaw: 0,
+        meanRaw: 0,
+        stdevRaw: 0,
+        sumSmooth: 0,
+        items: [],
+      },
+      setSmoothScores: (smoothScores: SmoothScores) => set({ smoothScores }),
+      excludedCasts: [],
+      addExcludedCast: (castHash: string) =>
+        set({ excludedCasts: [...get().excludedCasts, castHash] }),
+    }),
+    { name: "bear-store", storage: createJSONStorage(() => sessionStorage) }
+  )
+);
