ec5d324d4ce159710d75bd6dc73100add1294765
artlu99 <alu99.sm@gmail.com>
2025-03-09 09:24:19 -0400

    leaderboard visual

--- app/routes/winner.tsx
+++ app/routes/winner.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useFrame } from "~/components/context/FrameContext";
-import { pluralize } from "~/utils/pluralize";
 import { useBearStore } from "~/utils/zustand";
 
 const LOCAL_DEBUGGING = import.meta.env.DEV;
@@ -13,45 +12,80 @@ function Winner() {
   const { viewProfile, isInstalled, isNotificationsEnabled } = useFrame();
   const { winners } = useBearStore();
 
-  return isInstalled || LOCAL_DEBUGGING ? (
-    isNotificationsEnabled || LOCAL_DEBUGGING ? (
-      <div className="prose dark:prose-invert p-2">
-        <h2>Week March 4 ~ March 11</h2>
+  return (
+    <>
+      <div className="text-center text-warning text-xl">
+        Pre-launch payouts until{" "}
+        <span className="text-info font-bold">March 18</span>
+      </div>
 
-        <div>
-          <ol className="list-decimal pl-6 w-full max-w-full overflow-x-hidden text-xs">
-            {winners.map((winner) => {
-              const { fid, username, smoothScore, numCasts, payout } = winner;
-              return (
-                <li key={username}>
-                  <div className="block text-lg p-1 active:scale-95 transition-transform">
-                    <button
-                      type="button"
-                      className="link btn-link"
-                      onClick={() => viewProfile(fid, username)}
-                    >
-                      @{username}
-                    </button>
-                    -{smoothScore.toFixed(2)}-{pluralize(numCasts, "cast")}-$
-                    {payout.toFixed(2)}
+      {isInstalled || LOCAL_DEBUGGING ? (
+        isNotificationsEnabled || LOCAL_DEBUGGING ? (
+          <div className="prose dark:prose-invert p-2">
+            <h2 className="text-center mb-4">Week March 4 ~ March 11</h2>
+
+            <div className="flex flex-col gap-2">
+              {winners.map((winner, index) => {
+                const { fid, username, smoothScore, numCasts, payout } = winner;
+                return (
+                  <div
+                    key={username}
+                    className="card bg-base-100 shadow-sm rounded-md m-2"
+                  >
+                    <div className="card-body p-3">
+                      <div className="flex items-center gap-2">
+                        <div className="badge btn-soft badge-ghost">{index + 1}</div>
+                        <button
+                          type="button"
+                          className="text-lg font-medium hover:underline active:scale-95 transition-transform"
+                          onClick={() => viewProfile(fid, username)}
+                        >
+                          @{username}
+                        </button>
+                      </div>
+
+                      <div className="stats stats-horizontal w-full bg-transparent text-sm">
+                        <div className="stat px-2 py-1">
+                          <div className="stat-title text-xs opacity-60">
+                            Score🏆
+                          </div>
+                          <div className="stat-value text-md">
+                            {smoothScore.toFixed(2)}
+                          </div>
+                        </div>
+
+                        <div className="stat px-2 py-1">
+                          <div className="stat-title text-xs opacity-60">
+                            Casts 💅
+                          </div>
+                          <div className="stat-value text-md">{numCasts}</div>
+                        </div>
+
+                        <div className="stat px-2 py-1">
+                          <div className="stat-title text-xs opacity-60">
+                            💰Payout
+                          </div>
+                          <div className="stat-value text-md">
+                            ${payout.toFixed(2)}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </li>
-              );
-            })}
-          </ol>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className="text-error text-xl">
+            Please enable notifications to see payouts
+          </div>
+        )
+      ) : (
+        <div className="text-error text-xl">
+          Please install Frame to see payouts
         </div>
-        <div className="text-warning text-xl">
-          Payouts are fake until the game goes live!
-        </div>
-      </div>
-    ) : (
-      <div className="text-error text-xl">
-        Please enable notifications to see payouts
-      </div>
-    )
-  ) : (
-    <div className="text-error text-xl">
-      Please install Frame to see payouts
-    </div>
+      )}
+    </>
   );
 }
