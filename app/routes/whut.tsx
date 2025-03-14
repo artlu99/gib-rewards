1a97003219cfeafb0e278b8412f398d5e405819d
artlu99 <alu99.sm@gmail.com>
2025-03-14 12:48:53 -0400

    simple sign in and out

--- app/routes/whut.tsx
+++ app/routes/whut.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useFrame } from "~/components/context/FrameContext";
+import { useSignIn } from "~/hooks/use-sign-in";
 
 export const Route = createFileRoute("/whut")({
   component: Home,
@@ -7,31 +8,53 @@ export const Route = createFileRoute("/whut")({
 
 function Home() {
   const { openUrl } = useFrame();
+  const { logout, signIn, isSignedIn } = useSignIn();
 
   return (
-    <div className="prose dark:prose-invert p-8">
-      <h3>Contest 🏆</h3>
-      <p className="text-xl">
-        Get paid weekly for content that the most people want to see.
-      </p>
-      <p> </p>
-      <h3>SassyHash 💅</h3>
-      <p className="text-sm">
-        SassyHash leverages cryptographic building blocks and the open Farcaster
-        Protocol, to share restricted-distribution information in
-        self-sovereign, transparent, and sufficiently decentralized blah blah.
-      </p>
-      <p>
-        Read more
-        <button
-          onClick={() => openUrl("https://sassyhash.artlu.xyz/blog")}
-          type="button"
-          className="link link-secondary text-lg p-1 active:scale-95 transition-transform"
-        >
-          here
-        </button>
-        .
-      </p>
-    </div>
+    <>
+      <div className="prose dark:prose-invert p-8">
+        <h3>Contest 🏆</h3>
+        <p className="text-xl">
+          Get paid weekly for content that the most people want to see.
+        </p>
+        <p> </p>
+        <h3>SassyHash 💅</h3>
+        <p className="text-sm">
+          SassyHash leverages cryptographic building blocks and the open
+          Farcaster Protocol, to share restricted-distribution information in
+          self-sovereign, transparent, and sufficiently decentralized blah blah.
+        </p>
+        <p>
+          Read more
+          <button
+            onClick={() => openUrl("https://sassyhash.artlu.xyz/blog")}
+            type="button"
+            className="link link-secondary text-lg p-1 active:scale-95 transition-transform"
+          >
+            here
+          </button>
+          .
+        </p>
+      </div>
+      <div className="flex justify-center">
+        {isSignedIn ? (
+          <button
+            type="button"
+            className="btn btn-wide btn-secondary"
+            onClick={() => logout()}
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-wide btn-secondary"
+            onClick={() => signIn()}
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+    </>
   );
 }
